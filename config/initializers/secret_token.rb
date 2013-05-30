160ac1c9b1e850b8af7106c8f4f0f4ae6adbfbcf
Rubify <peter@rubify.com>
2013-05-30 10:57:18 +0700

    fix

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Visviet::Application.config.secret_token = '980a2b0bd34671517918e2c19c9d4d6134a0940195c573d0ded14e83ca70e7c6fe730a1851c02c257015a4712909aa9ba05157b5a365c9e0facd0b285978bde1'
